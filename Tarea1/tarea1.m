552351087ee724203e44ca6ba6cf85cd3cd23641
marioyeahs <marioyeahs@gmail.com>
2022-02-09 15:12:48 -0600

    add: docx file and load in string

---
+++ Tarea1/tarea1.m
@@ -0,0 +1,5 @@
+clc
+clear all
+close all
+
+str = extractFileText('/Users/jenniffercervantes/Documents/MATLAB/information-theory-course/Tarea1/Don Miguel Ruiz - Los Cuatro Acuerdos_ Una Guia Practica para la Libertad Personal (1999).docx');
