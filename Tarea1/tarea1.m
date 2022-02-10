7e0a087bb0a623b97e7aefd87f2cf20615da3f95
marioyeahs <marioyeahs@gmail.com>
2022-02-09 21:01:21 -0600

    add: alfabeto reducido

--- Tarea1/tarea1.m
+++ Tarea1/tarea1.m
@@ -7,10 +7,6 @@
 % En ASCII a-z|97-122, á-ú|(225 ,233,237,243,250), Á-Ú|(193,201,205,211,218),
 % A-Z|65-90
 % ' '|32,'.'|46, ','|44, '"'|34, '¿?'|63, '¡!'|33
-% a_z=[97:122];
-% a_u=[225 ,233,237,243,250];
-% A_U = [193 201 205 211 218];
-% A_Z = [65:90];
 
 clc
 clear all
@@ -26,5 +22,11 @@ chars_ascii = h.BinEdges; %caracteres en formato ascii
 chars_ascii=chars_ascii(1:end-1);
 [chars_ascii',prob'];
 % sum(prob)
+
+a_z = [97:122];
+a_u = [225 ,233,237,243,250];
+A_U = [193 201 205 211 218];
+A_Z = [65:90];
+alfabeto=sort([a_z a_u A_U A_Z]);
 
 
