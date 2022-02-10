235fd403b1cb22e383e8861c2251fa52bd9f933a
marioyeahs <marioyeahs@gmail.com>
2022-02-09 21:02:28 -0600

    fix: file path

--- Tarea1/tarea1.m
+++ Tarea1/tarea1.m
@@ -12,7 +12,7 @@ clc
 clear all
 close all
 
-str = extractFileText('/Users/jenniffercervantes/Documents/MATLAB/information-theory-course/Tarea1/Don Miguel Ruiz - Los Cuatro Acuerdos_ Una Guia Practica para la Libertad Personal (1999).docx');
+str = extractFileText('./Don Miguel Ruiz - Los Cuatro Acuerdos_ Una Guia Practica para la Libertad Personal (1999).docx');
 file_char = char(str);
 
 ascii_file = double(file_char);
