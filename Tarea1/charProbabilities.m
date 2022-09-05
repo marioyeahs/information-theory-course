d5682138068cdde8e06fdf9a3b1768f715e517aa
marioyeahs <marioyeahs@gmail.com>
2022-09-04 23:37:52 -0500

    add: cell array with pair of letters

--- Tarea1/charProbabilities.m
+++ Tarea1/charProbabilities.m
@@ -2,12 +2,12 @@ clc
 clear all
 close all
 
-C = extractFileText('./document.txt');
-Cc = double(C{:});                  % Convert Char To Double
-B = double('a':'z');      % Create Bin Ranges
-Hcts2 = histcounts(Cc,10);          % Do Histogram Counts
-CB = char(B);                       % Character Bins
-prob=Hcts2/length(Cc);
+text = extractFileText('./document.txt');
+str = double(text{:});                  % Convert Char To Double
+alph = double('a':'z');                % Create Bin Ranges
+Hcts2 = histcounts(str,10);          % Do Histogram Counts
+CB = char(alph);                       % Character Bins
+prob=Hcts2/length(str);
 suma=sum(prob);
 
 % matriz simétrica
@@ -21,8 +21,41 @@ dob=[];
 %matriz simetrica
 aa=0;
 for h=1:14830
-    if Cc(h)==Cc(h+1) && Cc(h)~=10
+    if str(h)==str(h+1) && str(h)~=10
         aa=aa+1;
-        dob = [dob char(Cc(h)) char(Cc(h+1))];
+        dob = [dob char(str(h)) char(str(h+1))];
     end
 end
+
+for i=str
+    if str(i)-str(i+1)==1
+        disp("mayot")
+    end
+end
+
+m_prob = zeros(27,27);
+for i=alph
+    for j=str
+        
+    end
+end
+
+pair = {};
+counter=1;
+% CB='abcdefghijl....'
+
+for j = CB
+    for i=CB
+        pair = [pair, strcat(j,i)];
+        counter = counter+1;
+    end
+end
+
+
+
+for p=pair
+    disp(count(str,p))
+end
+
+    
+
